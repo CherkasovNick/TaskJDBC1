f1cfbd0c7b2bd7f6535387c76be6131fc17a57f0
Nickolay <kolya_4erkasov@mail.ru>
2021-07-27 03:27:03 +0300

    git add git commit

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,115 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.SQLQuery;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
-    public UserDaoHibernateImpl() {
 
+    public UserDaoHibernateImpl() {
     }
 
 
     @Override
     public void createUsersTable() {
+        Session session = Util.getSessionFactory().openSession();
+        try {
+            session.beginTransaction();
+            SQLQuery query = session.createSQLQuery("CREATE TABLE usersjm(id INT NOT NULL AUTO_INCREMENT, name VARCHAR (20) NOT NULL, " +
+                    "lastname VARCHAR (20) NOT NULL, age INT, PRIMARY KEY (ID))");
+            query.executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+
 
     }
 
     @Override
     public void dropUsersTable() {
-
+        Session session = Util.getSessionFactory().openSession();
+        try {
+            session.beginTransaction();
+            SQLQuery query = session.createSQLQuery("DROP TABLE usersjm");
+            query.executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        Session session = Util.getSessionFactory().openSession();
+        try {
+            session.beginTransaction();
+            SQLQuery query = session.createSQLQuery("INSERT INTO usersjm(name, lastname, age) VALUES (\'" + name + "\', \'" + lastName + "\', " + age + ")");
+            query.executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+        Session session = Util.getSessionFactory().openSession();
+        try {
+            session.beginTransaction();
+            SQLQuery query = session.createSQLQuery("DELETE FROM usersjm WHERE id = " + id);
+            query.executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        Session session = Util.getSessionFactory().openSession();
+        List<User> list = new ArrayList<>();
+
+        try {
+            SQLQuery query = session.createSQLQuery("SELECT * FROM usersjm");
+            query.addEntity(User.class);
+            list = query.list();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+
+        return list;
     }
 
     @Override
     public void cleanUsersTable() {
-
+        Session session = Util.getSessionFactory().openSession();
+        try {
+            session.beginTransaction();
+            SQLQuery query = session.createSQLQuery("DELETE FROM usersjm");
+            query.executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
     }
 }
